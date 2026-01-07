0fc0f1168f44d05d3e2d7e6c8ab7f425b1466ae7
BrentMora <bomorales@up.edu.ph>
2026-01-07 16:17:37 +0800

    start new phase3

--- haskell-project/app/Main.hs
+++ haskell-project/app/Main.hs
@@ -1,16 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Main where
-
-import Client (mainClient)
-import Server (mainServer)
-import System.Environment (getArgs)
+module Main (main) where
 
 main :: IO ()
-main = do
-  args <- getArgs
-  print args
-  case args of
-    ["--host"] -> mainServer
-    [] -> mainClient "127.0.0.1" 15000
-    _ -> putStrLn "Usage: program        (start client)\n       program --host (start server)"
+main = putStrLn "Hello, Haskell!"
